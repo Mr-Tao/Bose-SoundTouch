1a4b090a43d1c0e55c89e9e3982d1ed26e977f3e
Lukáš Lipinský <6032558+Mr-Tao@users.noreply.github.com>
2026-08-26 01:20:55 +0200

    refactor(player): centralize static dependencies

--- pkg/service/soundtouchweb/static/js/components/NowPlaying.js
+++ pkg/service/soundtouchweb/static/js/components/NowPlaying.js
@@ -1,6 +1,4 @@
-import { h } from '/app/static/lib/preact.module.js';
-import { useState, useEffect, useRef } from '/app/static/lib/preact-hooks.module.js';
-import htm from '/app/static/lib/htm.module.js';
+import { h, htm, useEffect, useRef, useState } from '../dependencies.js';
 import { api } from '../api.js';
 
 const html = htm.bind(h);
---
+++ pkg/service/soundtouchweb/static/js/dependencies.js
@@ -0,0 +1,5 @@
+import { h, render } from '../lib/preact.module.js';
+import { useCallback, useEffect, useRef, useState } from '../lib/preact-hooks.module.js';
+import htm from '../lib/htm.module.js';
+
+export { h, htm, render, useCallback, useEffect, useRef, useState };
